2afcf333e8d2f5245f00b75b9c35d5118bb0355b
personaltamang011-blip <personaltamang011@gmail.com>
2025-12-08 19:57:54 -0800

    Update Google Sheet.js

--- Google Sheet.js
+++ Google Sheet.js
@@ -1,11 +1,11 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxjoTZMaxtqgITeTnktuODWfUFXgchfytszx2x19PhKkhA21IuRyKfFk2Pd_UZ6hb_c/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzFUwaJGsLC-GyDGWBP1GDs7_Fr6HGTJDdmtH5Znaz_BFPkrRgRe_Sn63e-3fXJL9TZ/exec'
 
 const form = document.forms['contact-form']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(response => alert("Form submitted successfully! ✅" ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
